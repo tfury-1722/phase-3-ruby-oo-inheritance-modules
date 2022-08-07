3c6435bd65be0dc06d1a17917b69e9f9b443a33f
christopher <cdnance1829@gmail.com>
2022-08-06 20:21:06 -0400

    finished

---
+++ lib/dance_module.rb
@@ -0,0 +1,18 @@
+module Dance
+    def twirl
+        "I'm twirling!"
+    end
+    
+    def jump
+        "Look how high I can jumping!"
+    end
+    
+    def pirouette
+        "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. It was a pleasure dancing for you all."
+    end
+    
+end
